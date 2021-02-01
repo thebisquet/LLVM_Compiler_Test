5b59d62e284c79d93c1203d05a00f4e9813b449a
Keinen Bousquet <keinen.bousquet@trojans.dsu.edu>
2021-02-01 08:43:36 -0600

    Attempt to sync changes to GitHub.

---
+++ Toy_Compiler_LLVM_Test/Toy_Compiler_LLVM_Test/Toy_Compiler_LLVM_Test.cpp
@@ -0,0 +1,6 @@
+// Toy_Compiler_LLVM_Test.cpp : This file contains the 'main' function. Program execution begins and ends here.//
+#include <iostream>
+
+int main() {
+    std::cout << "Hello World!\n";
+}
